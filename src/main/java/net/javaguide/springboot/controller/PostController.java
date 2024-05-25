7df30eda0fc3fd0bd3f55333908124f23a0749b6
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-05-25 12:09:16 +0700

    Create PostController.java

---
+++ src/main/java/net/javaguide/springboot/controller/PostController.java
@@ -0,0 +1,32 @@
+package net.javaguide.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.javaguide.springboot.dto.PostDto;
+import net.javaguide.springboot.service.PostService;
+
+@Controller
+@RequestMapping("/admin/posts")
+public class PostController {
+	
+	private PostService postService;
+	
+	public PostController(PostService postService) {
+		this.postService = postService;
+	}
+	
+	@GetMapping("")
+	public String getPosts(Model model) {
+		List<PostDto> posts = this.postService.findAllPosts();
+		
+		model.addAttribute("posts", posts);
+		
+		return "admin/posts";
+	}
+	
+}
