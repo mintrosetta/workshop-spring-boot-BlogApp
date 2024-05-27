c5bfb75a61a19ddc2c4bb4fd2efe2b23bfe1b587
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-05-27 11:53:36 +0700

    Update BlogController.java

--- src/main/java/net/javaguide/springboot/controller/BlogController.java
+++ src/main/java/net/javaguide/springboot/controller/BlogController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import net.javaguide.springboot.dto.PostDto;
 import net.javaguide.springboot.service.PostService;
@@ -36,5 +37,14 @@ public class BlogController {
 		model.addAttribute("post", postDto);
 		
 		return "blog/post-view";
+	}
+	
+	@GetMapping("search")
+	public String searchPosts(Model model, @RequestParam(value = "search") String search) {
+		List<PostDto> posts = this.postService.searchPost(search);
+		
+		model.addAttribute("posts", posts);
+		
+		return "blog/post-views";
 	}
 }
