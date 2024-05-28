cb8a32db57b7f5c9d9757d03af78679837b47a5c
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-05-28 11:50:37 +0700

    Create CommentRepository.java

---
+++ src/main/java/net/javaguide/springboot/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package net.javaguide.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.javaguide.springboot.entity.Comment;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+	
+}
