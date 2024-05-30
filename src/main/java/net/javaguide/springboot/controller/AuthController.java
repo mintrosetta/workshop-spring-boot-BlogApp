7f0b1fd6fc862e06d9da7987cf210ac8b984051b
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-05-30 15:40:19 +0700

    register page

--- src/main/java/net/javaguide/springboot/controller/AuthController.java
+++ src/main/java/net/javaguide/springboot/controller/AuthController.java
@@ -2,6 +2,7 @@ package net.javaguide.springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.javaguide.springboot.dto.RegisterDto;
@@ -9,10 +10,12 @@ import net.javaguide.springboot.dto.RegisterDto;
 @Controller
 @RequestMapping("auth")
 public class AuthController {
+	
+	@GetMapping("register")
 	public String showRegisterForm(Model model) {
-		RegisterDto registerDto = new RegisterDto();
+		RegisterDto user = new RegisterDto();
 		
-		model.addAttribute("register", registerDto);
+		model.addAttribute("user", user);
 		
 		return "auth/register";
 	}
