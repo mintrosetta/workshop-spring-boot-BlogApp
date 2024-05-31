2b7b276ca0376b5f8af0a4d20b7fe46b0614b599
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-05-31 23:13:37 +0700

    config role of path

--- src/main/java/net/javaguide/springboot/config/WebSpringSecurity.java
+++ src/main/java/net/javaguide/springboot/config/WebSpringSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity // enable spring security, can config security fetures
@@ -13,6 +14,10 @@ public class WebSpringSecurity {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     	http.csrf(csrf -> csrf.disable()) // csrf use only in bank
+    		.authorizeHttpRequests(authorize -> authorize
+    											.requestMatchers(new AntPathRequestMatcher("/resources/**")).permitAll()
+    											.requestMatchers("/auth/register/**").permitAll()
+    										    .requestMatchers(new AntPathRequestMatcher("/admin/**")).hasAnyRole("ADMIN", "GUEST"))
     		.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
     		.formLogin(form -> form.loginPage("/auth/login").loginProcessingUrl("/auth/login").defaultSuccessUrl("/admin/posts").permitAll())
     		.logout(config -> config.permitAll());
